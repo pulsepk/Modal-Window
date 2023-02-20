87ef9197b45c704ac49569b02f0d4c848a9670b8
pulsepk <hassannazar72@gmail.com>
2023-02-20 20:36:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const btnmodal = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+
+const openmodal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i=0; i<btnmodal.length; i++){
+    btnmodal[i].addEventListener('click', openmodal);
+}
+const closemodal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+close.addEventListener('click', closemodal);
